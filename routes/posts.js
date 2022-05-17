a85e60163330d58f3c897a8855c2a678390b27de
Vic <syuan0212@gmail.com>
2022-05-17 12:04:49 +0800

    fix posts API and router

--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const upload = require('../service/upload');
-const handleErrorAsync = require("../service/handleErrorAsync");
-const PostsControllers = require("../controllers/posts");
+const postsControl = require("../controllers/posts");
 const { isAuth } = require("../service/auth");
 
-router.get('/', isAuth, handleErrorAsync((req, res, next) => PostsControllers.getPosts(req, res, next)));
-router.post('/', isAuth, upload.array('photos', 10), handleErrorAsync((req, res, next) => PostsControllers.postPost(req, res, next)));
-router.patch('/:id', isAuth, handleErrorAsync((req, res, next) => PostsControllers.patchPost(req, res, next)));
+router.get('/', isAuth, postsControl.getPosts);
+router.get('/:id', isAuth, postsControl.getPost);
+router.post('/', isAuth, upload.array('photos', 10), postsControl.postPost);
+router.patch('/:id', isAuth, postsControl.patchPost);
+router.delete('/delete/:id', isAuth, postsControl.deletePost);
+
+// ＊＊＊測試用＊＊＊ 刪除所有貼文資料
+router.delete('/deleteAll', postsControl.deletePosts);
 
 module.exports = router;
