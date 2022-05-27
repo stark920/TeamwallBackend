9687c83fa88c0074f235669ad65e21856502f6d6
Genos Huang <38322680+stark920@users.noreply.github.com>
2022-05-27 15:25:36 +0800

    feat: mail service

---
+++ service/email.js
@@ -0,0 +1,26 @@
+const nodemailer = require('nodemailer');
+
+const config = {
+  service: 'gmail',
+  auth: {
+    type: 'OAuth2',
+    user: process.env.GMAIL_USERNAME,
+    pass: process.env.GMAIL_PASSWORD,
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+  },
+};
+// 建立SMTP客戶端物件
+const transporter = nodemailer.createTransport(config);
+
+// 傳送郵件
+const sendMail = (mail) => {
+  return new Promise((resolve, reject) => {
+    transporter.sendMail(mail, function (error, info) {
+      error ? reject(error) : resolve(info.response);
+    });
+  });
+};
+
+module.exports = sendMail;
