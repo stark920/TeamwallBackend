bd4eb21ab1caf8e3c8927de6dae74681963a120c
joe <joe.chang1014@gmail.com>
2022-06-03 21:07:49 +0800

    fix: 修正聊天記錄

--- service/socket.js
+++ service/socket.js
@@ -1,32 +1,32 @@
-const { Server } = require('socket.io');
-const jwt = require('jsonwebtoken');
-const dotenv = require('dotenv');
-const ChatRoom = require('../models/chatRoomModel');
-const User = require('../models/userModel');
-const console = require('./console');
+const { Server } = require("socket.io");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+const ChatRoom = require("../models/chatRoomModel");
+const User = require("../models/userModel");
+const console = require("./console");
 
-dotenv.config({ path: '../config.env' });
+dotenv.config({ path: "../config.env" });
 
 module.exports = (server) => {
-  const idPath = '_id';
+  const idPath = "_id";
   const io = new Server(server, {
-    path: '/socket.io/',
+    path: "/socket.io/",
     cors: {
-      origin: '*',
+      origin: "*",
     },
   });
 
   // 驗證token
   io.use(async (socket, next) => {
-    const token = socket.handshake.query?.token;
+    const token = socket.handshake.auth?.token;
     if (!token) {
-      return next(new Error('請重新登入'));
+      return next(new Error("請重新登入"));
     }
 
     const decodedData = await new Promise((resolve, reject) => {
       jwt.verify(token, process.env.JWT_SECRET, (error, payload) => {
         if (error) {
-          reject(next(new Error('請重新登入')));
+          reject(next(new Error("請重新登入")));
         } else {
           resolve(payload);
         }
@@ -53,104 +53,107 @@ module.exports = (server) => {
     return currentUser?.[idPath];
   };
 
+  const getHistory = async (room, lastTime) => {
+    let msgList = [];
+    if (lastTime) {
+      const [queryResult] = await ChatRoom.aggregate([
+        { $match: { $expr: { $eq: ["$_id", { $toObjectId: room }] } } },
+        {
+          $project: {
+            messages: 1,
+          },
+        },
+        {
+          $project: {
+            messages: {
+              $slice: [
+                {
+                  $filter: {
+                    input: "$messages",
+                    as: "item",
+                    cond: {
+                      $lt: ["$$item.createdAt", new Date(lastTime)],
+                    },
+                  },
+                },
+                30,
+              ],
+            },
+          },
+        },
+      ]);
+      msgList = queryResult.messages;
+    } else {
+      msgList = await ChatRoom.find(
+        { _id: room },
+        { messages: { $slice: -30 } }
+      );
+      msgList = msgList[0]?.messages;
+    }
+    return msgList;
+  };
+
   // 建立連接
-  io.of('/chat').on('connection', async (socket) => {
+  io.of("/chat").on("connection", async (socket) => {
     const room = socket.handshake.query?.room;
-    const token = socket.handshake.query?.token;
-    console.log('connection----', room);
+    const token = socket.handshake.auth?.token;
+    let userId = await getUserId(token);
+    userId = userId.toString();
+    console.log("connection----", room);
     if (room) {
       socket.join(room);
     }
 
-    let userId = await getUserId(token);
-    userId = userId.toString();
-
+    const msgList = await getHistory(room);
+    socket.emit("history", msgList);
     socket.use(([payload], next) => {
-      console.log('payload', payload);
+      console.log("payload", payload);
       if (payload?.message?.length > 100) {
-        return next(new Error('您輸入的內容過長'));
+        return next(new Error("您輸入的內容過長"));
       }
       return next();
     });
 
     // 監聽 client發來的訊息
-    socket.on('chatMessage', async (msg) => {
+    socket.on("chatMessage", async (msg) => {
       const { message } = msg;
       const createdAt = Date.now();
       await ChatRoom.findByIdAndUpdate(room, {
         $push: { messages: { sender: userId, message, createdAt } },
       });
       // 針對該房間廣播訊息
-      io.of('/chat')
+      io.of("/chat")
         .to(room)
-        .emit('chatMessage', { message, sender: userId, createdAt });
-      console.log('userInfo', room, userId);
-      console.log('傳來的訊息', msg);
+        .emit("chatMessage", { message, sender: userId, createdAt });
     });
 
     // 使用者輸入中
-    socket.on('typing', (boolean) => {
-      socket.broadcast.in(room).emit('typing', boolean);
+    socket.on("typing", (boolean) => {
+      socket.broadcast.in(room).emit("typing", boolean);
     });
 
     // 歷史訊息
-    socket.on('history', async (info) => {
-      console.log('history', info, room);
+    socket.on("history", async (info) => {
       const { lastTime } = info;
-      let msgList = [];
-      if (lastTime) {
-        console.log('lastTime', lastTime);
-        const [queryResult] = await ChatRoom.aggregate([
-          { $match: { $expr: { $eq: ['$_id', { $toObjectId: room }] } } },
-          {
-            $project: {
-              messages: 1,
-            },
-          },
-          {
-            $project: {
-              messages: {
-                $slice: [
-                  {
-                    $filter: {
-                      input: '$messages',
-                      as: 'item',
-                      cond: {
-                        $lt: ['$$item.createdAt', new Date(lastTime)],
-                      },
-                    },
-                  },
-                  30,
-                ],
-              },
-            },
-          },
-        ]);
-        msgList = queryResult.messages;
-      } else {
-        msgList = await ChatRoom.find(
-          { _id: room },
-          { messages: { $slice: -30 } },
-        );
-        msgList = msgList[0]?.messages;
-      }
-      socket.emit('history', msgList);
+      const msgList = await getHistory(room, lastTime);
+      socket.emit("history", msgList);
     });
-    socket.on('leaveRoom', (currentRoom) => {
-      console.log('leaveRoom~~~', currentRoom);
+
+    socket.on("leaveRoom", (currentRoom) => {
+      console.log("leaveRoom~~~", currentRoom);
       socket.leave(currentRoom);
     });
     // 錯誤處理
-    socket.on('error', (err) => {
-      socket.emit('error', err.message);
+    socket.on("error", (err) => {
+      socket.emit("error", err.message);
     });
     // 斷開連接
-    socket.on('disconnect', (currentSocket) => {
-      console.log('socket-disconnect', currentSocket);
+    socket.on("disconnect", (currentSocket) => {
+      console.log("socket-disconnect", currentSocket);
     });
   });
 
-  io.of('/chat').on('connect_error', (err) => {
+  io.of("/chat").on("connect_error", (err) => {
     console.log(`connect_error due to ${err.message}`);
   });
 
